d3b2d40dc7cc420472d9b8dfa61dd83efac03ecb
dhuruvandb <d.b.dhuruvan2000@gmail.com>
2023-02-28 23:21:37 +0530

    Added hr,min,sec,ms in a object

--- Stopwatch.js
+++ Stopwatch.js
@@ -2,52 +2,72 @@ let p = document.querySelector("p");
 let start = document.querySelectorAll("button")[0];
 let pause = document.querySelectorAll("button")[1];
 let reset = document.querySelectorAll("button")[2];
+pause.style.display="none";
+reset.style.display="none";
 let milliseconds=0;
 let seconds=00;
 let minutes=00;
 let hour=00;
 
 
-let timer = ()=>{
+let timer = {
 
-setInterval(function(){
+ms:()=>{setInterval(function(){
 milliseconds++;
-if(milliseconds===60){
-	milliseconds=00;
-}
-p.innerHTML=`${hour}:${minutes}:${seconds}:${milliseconds}`
-},150)
+p.innerHTML=`${hour}:${minutes}:${seconds}:${milliseconds}`.padStart(2, '0');
 
-setInterval(function(){
+if(milliseconds===60){
+milliseconds=0;
+}
+
+
+},150)},
+sec:()=>{setInterval(function(){
 seconds++;
+p.innerHTML=`${hour}:${minutes}:${seconds}:${milliseconds}`
 if(seconds===60){
 	seconds=00;
 }
-p.innerHTML=`${hour}:${minutes}:${seconds}:${milliseconds}`
-},1000)
 
-setInterval(function(){
+},1000)},
+
+min:()=>{setInterval(function(){
 minutes++;
 if(minutes===60){
 	minutes=00;
 }
 p.innerHTML=`${hour}:${minutes}:${seconds}:${milliseconds}`
-},60000)
+},60000)},
 
-setInterval(function(){
+hour:()=>{setInterval(function(){
 hour++;
 p.innerHTML=`${hour}:${minutes}:${seconds}:${milliseconds}`
-},360000)
+},360000)}
 
 }
 
-start.addEventListener("click",timer)
+start.addEventListener("click",()=>{
+
+timer.ms()
+timer.sec()
+timer.min()
+timer.hour()
+pause.style.display="inline-block";
+reset.style.display="inline-block";
+})
 
 
 pause.addEventListener("click",()=>{alert("Click Ok to continue")})
 
 reset.addEventListener("click",()=>{ 
-clearInterval(timer)
+
+clearInterval(()=>{
+timer.ms()
+timer.sec()
+timer.min()
+timer.hour()
+})
+
 milliseconds=00;
 seconds=00;
 minutes=00;
