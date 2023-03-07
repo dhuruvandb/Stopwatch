580dcc3ef51dd172c97b3f50f262fa0a7b4cbf00
dhuruvandb <d.b.dhuruvan2000@gmail.com>
2023-03-07 23:16:28 +0530

    pause and reset button working need to work on 00:00:00:00

--- Stopwatch.js
+++ Stopwatch.js
@@ -5,72 +5,100 @@ let reset = document.querySelectorAll("button")[2];
 pause.style.display="none";
 reset.style.display="none";
 let milliseconds=0;
-let seconds=00;
-let minutes=00;
-let hour=00;
+let seconds=0;
+let minutes=0;
+let hour=0;
+let ms;
+let sec;
+let min;
+let hr;
+let is_pause = false;
 
+function Stopwatch(){
 
-let timer = {
-
-ms:()=>{setInterval(function(){
+ms = setInterval(function(){
 milliseconds++;
-p.innerHTML=`${hour}:${minutes}:${seconds}:${milliseconds}`.padStart(2, '0');
 
+p.innerHTML=`0${hour}:0${minutes}:0${seconds}:0${milliseconds}`
+
+if(milliseconds>9){
+p.innerHTML=`0${hour}:0${minutes}:0${seconds}:${milliseconds}`
+}
 if(milliseconds===60){
 milliseconds=0;
-}
+p.innerHTML=`0${hour}:0${minutes}:0${seconds}:0${milliseconds}`}
+},150)
 
-
-},150)},
-sec:()=>{setInterval(function(){
+sec=setInterval(function(){
 seconds++;
-p.innerHTML=`${hour}:${minutes}:${seconds}:${milliseconds}`
+p.innerHTML=`0${hour}:0${minutes}:${seconds}:0${milliseconds}`
+
+if(seconds>9){
+	p.innerHTML=`0${hour}:0${minutes}:${seconds}:0${milliseconds}`
+}
+
 if(seconds===60){
-	seconds=00;
+	seconds=0;
+p.innerHTML=`0${hour}:0${minutes}:${seconds}:0${milliseconds}`
+
 }
 
-},1000)},
+},1000)
 
-min:()=>{setInterval(function(){
+
+min=
+setInterval(function(){
 minutes++;
+
 if(minutes===60){
-	minutes=00;
+	minutes=0;
 }
-p.innerHTML=`${hour}:${minutes}:${seconds}:${milliseconds}`
-},60000)},
+p.innerHTML=`0${hour}:0${minutes}:${seconds}:0${milliseconds}`
+},60000)
 
-hour:()=>{setInterval(function(){
+
+
+hr=setInterval(function(){
 hour++;
-p.innerHTML=`${hour}:${minutes}:${seconds}:${milliseconds}`
-},360000)}
-
+p.innerHTML=`0${hour}:0${minutes}:${seconds}:0${milliseconds}`
+},360000)
 }
+
 
 start.addEventListener("click",()=>{
-
-timer.ms()
-timer.sec()
-timer.min()
-timer.hour()
+Stopwatch();
+start.style.display="none"
 pause.style.display="inline-block";
 reset.style.display="inline-block";
 })
 
 
-pause.addEventListener("click",()=>{alert("Click Ok to continue")})
+let s1 = ()=>{ 
 
-reset.addEventListener("click",()=>{ 
+clearInterval(ms)
+clearInterval(hr)
+clearInterval(min)
+clearInterval(sec)
 
-clearInterval(()=>{
-timer.ms()
-timer.sec()
-timer.min()
-timer.hour()
+}
+
+
+
+pause.addEventListener("click",()=>{
+if(!is_pause){
+	s1();	
+}
+if(is_pause){
+Stopwatch();
+}
+is_pause=!is_pause;
 })
 
-milliseconds=00;
-seconds=00;
-minutes=00;
-let hour=00;
+
+reset.addEventListener("click",()=>{
+s1();
+start.style.display="inline-block"
+pause.style.display="none";
+p.innerHTML=`00:00:00:00`
 
 })
